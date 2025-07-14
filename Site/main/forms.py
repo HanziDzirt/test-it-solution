8c6e37bbbdfd066cc8f9925307ff64fe3426c46d
Hanzi <asafger31@gmail.com>
2025-07-15 02:36:38 +0300

    V0.3

--- Site/main/forms.py
+++ Site/main/forms.py
@@ -1,17 +1,18 @@
 from .models import Citation, Source
 from django import forms
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea,  NumberInput
 
 class CitationForm(ModelForm):
     class Meta:
         model = Citation
-        fields = ['source', 'text']
+        fields = ['source', 'text', 'weight']
         widgets = {
-
             'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Цитата'}),
             'source': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Источник'}),
-
+            'weight': NumberInput(attrs={'class': 'form-control', 'placeholder': 'Вес', 'min': 0.1, 'step': 0.1}),
         }
+
+
 class SourceForm(ModelForm):
     class Meta:
         model = Source
--- Site/main/views.py
+++ Site/main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from .forms import CitationForm
-from .models import Citation
+from .models import Citation, Source
 import random
 from django.db.models import ExpressionWrapper, FloatField, F
 from .forms import CitationForm, SourceForm
+from django.contrib import messages
 
 def home(request):
     citations = Citation.objects.all()
@@ -22,23 +22,43 @@ def top10(request):
     ).order_by('-rating')[:10]
     return render(request, 'main/top10.html', {'title': 'Десять лучших цитат', 'citations': top_list})
 
+
 def add(request):
     error = ''
-    if request.method == 'POST':
-        formC = CitationForm(request.POST)
-        if form.is_valid():
-            form.save()
-        formS = SourceForm(request.POST)
-        if form.is_valid():
-            form.save()
-            redirect('home')
-        else:
-            error = 'ERROR'
     formC = CitationForm()
     formS = SourceForm()
+    if request.method == 'POST':
+        formC = CitationForm(request.POST)
+        if formC.is_valid():
+            text = formC.cleaned_data['text']
+            source = formC.cleaned_data['source']
+            if Citation.objects.filter(text=text).exists():
+                messages.error(request, 'Цитата уже существует!')
+            elif Citation.objects.filter(source=source).count() >= 3:
+                messages.error(request, 'У источника максимум 3 цитаты!')
+            else:
+                formC.save()
+                messages.success(request, 'Цитата добавлена!')
+                return redirect('/')
+        else:
+            error = 'Что-то пошло не так'
+
+        formS = SourceForm(request.POST)
+        if formS.is_valid():
+            name = formS.cleaned_data['name']
+            if Source.objects.filter(name=name).exists():
+                messages.error(request, 'Источник уже существует!')
+            else:
+                formS.save()
+                messages.success(request, 'Источник добавлен!')
+                return redirect('/')
+        else:
+            error = 'Что-то пошло не так'
+
     context = {
         'formC': formC,
         'formS': formS,
         'error': error,
+        'title': 'Добавление цитаты',
     }
-    return render(request, 'main/add.html',{'title': 'Добавление цитаты'})
+    return render(request, 'main/add.html', context)
